4b8a4c7f8a7b320058d0a49a437e8fb7ce988d5c
Ekaterina <kat-erina-07@mail.ru>
2023-07-05 14:21:03 +0400

    Number position

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,111 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 50. Напишите программу, которая на вход принимает позиции элемента
+в двумерном массиве, и возвращает значение этого элемента или же указание,
+что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет
+*/
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+int[,] Create2DArray(int rows, int cols)
+{
+    return new int[rows, cols];
+}
+void Fill2DArray(int[,] array, int min, int max)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = rnd.Next(min, max + 1);
+}
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]}\t");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+void PosNumber(int[,] array, int pos1, int pos2)
+{
+    if (pos1 > array.GetLength(0) || pos1 < 1 || pos2 < 1 || pos2 > array.GetLength(1))
+        Console.WriteLine("Такого элемента в массиве нет");
+    else
+        Console.WriteLine($"Элемент с позицией ({pos1}, {pos2}) --> {array[pos1 - 1, pos2 - 1]}");
+}
+
+int rows = InputNum("Введите количество строк: ");
+int columns = InputNum("Введите количество столбцов: ");
+int minValue = InputNum("Введите минимальное значение диапазона: ");
+int maxValue = InputNum("Введите максимальное значение диапазона: ");
+int posrows = InputNum("Введите порядковый номер элемента в строке: ");
+int poscols = InputNum("Введите порядковый номер элемента в столбце: ");
+int[,] myArray = Create2DArray(rows, columns);
+Fill2DArray(myArray, minValue, maxValue);
+Print2DArray(myArray);
+PosNumber(myArray, posrows, poscols);
+
+/*
+Второй вариант порядковый номер элемента
+*/
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+int[,] Create2DArray(int rows, int cols)
+{
+    return new int[rows, cols];
+}
+void Fill2DArray(int[,] array, int min, int max)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = rnd.Next(min, max + 1);
+}
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]}\t");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+void PosNumber(int[,] array, int pos)
+{
+    if (pos <= array.GetLength(0) * array.GetLength(1) && pos >= 1)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                if ((array.GetLength(1) * i) + j + 1 == pos)
+                    Console.WriteLine($"Элемент на позиции {pos} равно {array[i, j]}");
+            }
+        }
+    }
+    else
+        Console.WriteLine("Такого элемента в массиве нет");
+}
+
+int rows = InputNum("Введите количество строк: ");
+int columns = InputNum("Введите количество столбцов: ");
+int minValue = InputNum("Введите минимальное значение диапазона: ");
+int maxValue = InputNum("Введите максимальное значение диапазона: ");
+int num = InputNum("Введите позицию элемента: ");
+int[,] myArray = Create2DArray(rows, columns);
+Fill2DArray(myArray, minValue, maxValue);
+Print2DArray(myArray);
+PosNumber(myArray, num);
